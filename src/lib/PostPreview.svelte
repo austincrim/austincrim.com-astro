78ef0ea575cef4eaf4d1125cda53e211c7255dee
Austin Crim <aust.crim@gmail.com>
2022-08-22 17:07:12 -0500

    fancy border

--- src/lib/PostPreview.svelte
+++ src/lib/PostPreview.svelte
@@ -2,12 +2,12 @@
   export let post
 </script>
 
-<a class="group" href={`${post.url}`}>
+<a class="group relative" href={`${post.url}`}>
   <article
-    class="relative flex items-center transition-all ease-out mouse:group-hover:-translate-y-1 mouse:group-active:translate-y-0 mouse:group-active:shadow-none mouse:group-hover:shadow-md group-focus-visible:-translate-y-1"
+    class="relative z-10 flex items-center transition-all ease-out mouse:group-hover:-translate-y-1 duration-100 mouse:group-active:translate-y-0 group-focus-visible:-translate-y-1"
   >
     <div
-      class="flex flex-col flex-grow p-8 space-y-1 text-base border border-gray-200 rounded shadow bg-off-base"
+      class="flex flex-col flex-grow p-8 space-y-1 text-base border border-gray-200 rounded bg-off-base"
     >
       <div
         class="flex flex-col justify-between space-y-2 md:space-y-0 md:space-x-2 md:flex-row md:items-baseline"
@@ -24,4 +24,7 @@
       <p class="max-w-3xl leading-8">{post.lede}</p>
     </div>
   </article>
+  <div
+    class="w-full h-full translate-y-1 fancy-gradient shadow absolute rounded bg-red-500 top-0 left-0"
+  />
 </a>
