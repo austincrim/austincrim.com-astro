98524106cf87685ea22cf083bd0c0043a8c96190
Austin Crim <aust.crim@gmail.com>
2022-08-25 21:34:20 -0500

    fix text color :/

--- src/lib/PostPreview.svelte
+++ src/lib/PostPreview.svelte
@@ -2,7 +2,7 @@
   export let post
 </script>
 
-<a class="group relative" href={`${post.url}`}>
+<a class="group relative text-gray-700" href={`${post.url}`}>
   <article
     class="relative z-10 flex items-center transition-all ease-out mouse:group-hover:-translate-y-1 duration-100 mouse:group-active:translate-y-0 group-focus-visible:-translate-y-1"
   >
