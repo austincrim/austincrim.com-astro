53c72e3f44c5b19b517e89d9c1ebf51c90a9ba0f
Austin Crim <aust.crim@gmail.com>
2023-04-17 09:58:11 -0500

    hello again bouncy nav

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,28 +1,71 @@
 <script>
   import { GitHubLogo, Rss, TwitterLogo, YoutubeLogo } from './Icons'
+  import { spring } from 'svelte/motion'
 
   export let activeLink
+
+  let activeBorderPosition = spring(-10, {
+      stiffness: 0.13,
+      damping: 0.3,
+      precision: 0.1
+    }),
+    linkEls = {},
+    linkBorderEl,
+    navEl
+  $: {
+    if (linkBorderEl) {
+      activeBorderPosition.set(
+        linkEls[activeLink].getBoundingClientRect().x -
+          navEl.getBoundingClientRect().x
+      )
+      linkBorderEl.style.width = `${
+        linkEls[activeLink].getBoundingClientRect().width
+      }px`
+    }
+  }
+
+  function nudgeBorder(link, value) {
+    if (link === activeLink) return
+
+    activeBorderPosition.update((pos) => pos + value)
+  }
 </script>
 
-<nav class="pb-10 relative">
-  <div class="flex items-end justify-between">
+<nav
+  bind:this={navEl}
+  class="text-muted lg:max-w-5xl md:mx-auto md:max-w-3xl relative"
+>
+  <div class="flex items-baseline justify-between">
     <div class="text-xl">
       <a
-        class:active={activeLink === 'home'}
-        class="text-gray-700 inline-block transition-colors hover:text-gray-500 mr-6"
+        bind:this={linkEls['home']}
+        class="inline-block transition-colors hover:text-muted-hover mr-6 md:mr-14"
         href="/"
+        on:mouseenter={() => nudgeBorder('home', -10)}
+        on:focus={() => nudgeBorder('home', -10)}
+        on:mouseout={() => nudgeBorder('home', 10)}
+        on:blur={() => nudgeBorder('home', 10)}
       >
         Home
       </a>
       <a
-        class:active={activeLink === 'blog'}
-        class="text-gray-700 inline-block transition-colors hover:text-gray-500 mr-4"
+        bind:this={linkEls['blog']}
+        class="inline-block transition-colors hover:text-muted-hover"
         href="/blog"
+        on:mouseenter={() => nudgeBorder('blog', 10)}
+        on:focus={() => nudgeBorder('blog', 10)}
+        on:mouseout={() => nudgeBorder('blog', -10)}
+        on:blur={() => nudgeBorder('blog', -10)}
       >
         Blog
       </a>
+      <div
+        bind:this={linkBorderEl}
+        style="transform: translatex({$activeBorderPosition}px)"
+        class="linkBorder transition-[width] absolute -bottom-2 left-0 h-[2px] bg-blue-500"
+      />
     </div>
-    <div class="flex items-baseline space-x-6 sm:space-x-8">
+    <div class="flex items-baseline space-x-8">
       <a
         aria-label="austin crim's rss feed"
         href="/rss.xml"
@@ -33,14 +76,14 @@
       <a
         aria-label="austin crim's github profile"
         href="https://github.com/austincrim"
-        class="inline-block w-8 h-8 transition-colors hover:text-gray-600"
+        class="inline-block w-8 h-8 transition-colors hover:text-muted-hover"
       >
         {@html GitHubLogo()}
       </a>
       <a
         aria-label="austin crim's twitter profile"
         href="https://twitter.com/crim_codes"
-        class="inline-block w-8 h-8 transition-colors text-[#1D9BF0] hover:text-[#2EACF1] "
+        class="inline-block w-8 h-8 transition-colors text-[#1D9BF0] hover:text-[#2EACF1]"
       >
         {@html TwitterLogo()}
       </a>
@@ -54,9 +97,3 @@
     </div>
   </div>
 </nav>
-
-<style>
-  .active {
-    @apply border-b-2 border-b-blue-500 pb-1;
-  }
-</style>
