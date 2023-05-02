cf8ee54c5af8d62d5fdc61321a61fcc4c2e15f42
Austin Crim <aust.crim@gmail.com>
2023-05-01 22:26:17 -0500

    tweaks

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,11 +1,10 @@
 import { defineConfig } from 'astro/config'
 import svelte from '@astrojs/svelte'
 import tailwind from '@astrojs/tailwind'
-import mdx from '@astrojs/mdx'
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [svelte(), tailwind(), mdx()],
+  integrations: [svelte(), tailwind()],
   markdown: {
     shikiConfig: {
       theme: 'github-light'
